a6ec068ce6beebf5f762d59dd6152fe35ce1fcb4
jacobleesinger <jacobleesinger@gmail.com>
2021-02-10 15:54:14 -0800

    [IssuerDataService] add issuer data service

---
+++ src/services/data/issuer.data.service.ts
@@ -0,0 +1,21 @@
+import createService, { Service as MikroOrmService } from 'feathers-mikro-orm';
+import { ServiceAddons } from '@feathersjs/feathers';
+
+import { Application } from '../../declarations';
+import { IssuerEntity } from '../../entities/Issuer';
+
+declare module '../../declarations' {
+  interface ServiceTypes {
+    issuerData: MikroOrmService & ServiceAddons<IssuerEntity>;
+  }
+}
+
+export default function (app: Application): void {
+  const userDataService = createService({
+    Entity: IssuerEntity,
+    name: 'IssuerEntity',
+    orm: app.get('orm')
+  });
+
+  app.use('/issuerData', userDataService);
+}
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,9 @@
 import { Application } from '../declarations';
 import userData from './data/user.data.service';
+import issuerData from './data/issuer.data.service';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 export default function (app: Application): void {
   app.configure(userData);
+  app.configure(issuerData);
 }
