c222025582b1a83aafe25b331fe5b2114452f9a8
jacobleesinger <jacobleesinger@gmail.com>
2021-02-17 11:47:46 -0800

    [app.hooks] add logging hooks

--- src/app.hooks.ts
+++ src/app.hooks.ts
@@ -1,9 +1,46 @@
-// Application hooks that run for every service
-// Don't remove this comment. It's needed to format import lines nicely.
+import { HookContext } from '@feathersjs/feathers';
+import { pick } from 'lodash';
+
+import logger from './logger';
+import { sanitizeValues } from './utils/sanitizeValues';
+
+function logError (ctx: HookContext): void {
+  const { path, method, error, data } = ctx;
+  const { name, code, message, stack } = error;
+  const sanitizedData = sanitizeValues(data, ['password']);
+  const rawInfo = pick(ctx, ['params', 'id']);
+  const info = { ...rawInfo, data: sanitizedData, stack };
+
+  if (info.params?.headers?.authorization) {
+    info.params.headers.authorization = 'Bearer *****';
+  }
+
+  if (info.params?.authToken) {
+    info.params.authToken = '*****';
+  }
+
+  logger.warn(`Error in ${path}#${method}: name=${name} code=${code} message=${message}`, info);
+}
+
+function log (ctx: HookContext): void {
+  const { path, method, id, data } = ctx;
+  const sanitizedData = sanitizeValues(data, ['password']);
+  logger.info(`${path}#${method}${id ? ` id: ${id}` : ''}${data ? ` data: ${JSON.stringify(sanitizedData)}}` : ''}`);
+}
+
+function logResult (ctx: HookContext): HookContext {
+  const { path, method, result } = ctx;
+  const string = JSON.stringify(result);
+  const length = 1500; // prevent exceedingly long result log messages.
+  const resultString = string.length < length ? string : string.substring(0, length - 3) + '...';
+
+  logger.info(`${path}#${method} result: ${resultString}`);
+  return ctx;
+}
 
 export default {
   before: {
-    all: [],
+    all: [log],
     find: [],
     get: [],
     create: [],
@@ -13,7 +50,7 @@ export default {
   },
 
   after: {
-    all: [],
+    all: [logResult],
     find: [],
     get: [],
     create: [],
@@ -23,7 +60,7 @@ export default {
   },
 
   error: {
-    all: [],
+    all: [logError],
     find: [],
     get: [],
     create: [],
