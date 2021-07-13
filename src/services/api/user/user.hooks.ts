854509f167bbb7b98dc2ee8466726315c968fe49
Ray Smets <rayjsmets@gmail.com>
2021-07-13 13:59:30 -0700

    [Credential Issueing] simplified.

--- src/services/api/user/user.hooks.ts
+++ src/services/api/user/user.hooks.ts
@@ -3,7 +3,7 @@ import { Hook } from '@feathersjs/feathers';
 import { convertCredentialSubject, issueCredential as sdkIssueCredential, UnumDto } from '@unumid/server-sdk';
 import { issueCredential as sdkIssueCredentialDeprecatedV2 } from '@unumid/server-sdk-deprecated-v2';
 import { issueCredential as sdkIssueCredentialDeprecatedV1 } from '@unumid/server-sdk-deprecated-v1';
-import { Credential, CredentialPb, CredentialSubject, Proof, ProofPb } from '@unumid/types';
+import { CredentialPb, CredentialSubject, Proof, ProofPb } from '@unumid/types';
 import { Credential as CredentialDeprecatedV2 } from '@unumid/types-deprecated-v2';
 import { Credential as CredentialDeprecatedV1 } from '@unumid/types-deprecated-v1';
 import { Service as MikroOrmService } from 'feathers-mikro-orm';
@@ -131,8 +131,7 @@ export const issueCredential = async (
 export const issueCredentialV3 = async (
   issuerEntity: IssuerEntity,
   credentialSubject: CredentialSubject,
-  credentialType: string,
-  version: string
+  credentialType: string
 ): Promise<UnumDto<CredentialPb>> => {
   let authCredentialResponse;
 
@@ -173,7 +172,7 @@ export const convertUnumDtoToCredentialEntityOptions = (issuerDto: UnumDto<Crede
   };
 };
 
-export const convertUnumDtoToCredentialEntityOptionsV3 = (issuerDto: UnumDto<CredentialPb>, version: string): CredentialEntityOptions => {
+export const convertUnumDtoToCredentialEntityOptionsV3 = (issuerDto: UnumDto<CredentialPb>): CredentialEntityOptions => {
   const proof: ProofPb = {
     ...issuerDto.body.proof,
     created: issuerDto.body.proof?.created,
@@ -217,16 +216,6 @@ export const getDefaultIssuerEntity: UserServiceHook = async (ctx) => {
 };
 
 export const issueAuthCredential: UserServiceHook = async (ctx) => {
-  const { params } = ctx;
-  return issueAuthCredentialV3(ctx);
-  // if (lt(params.headers?.version, '3.0.0')) {
-  //   return issueAuthCredentialV2(ctx);
-  // } else {
-  //   return issueAuthCredentialV3(ctx);
-  // }
-};
-
-export const issueAuthCredentialV2: UserServiceHook = async (ctx) => {
   const { id, data, result, params } = ctx;
   const defaultIssuerEntity = params.defaultIssuerEntity as IssuerEntity;
 
@@ -244,72 +233,13 @@ export const issueAuthCredentialV2: UserServiceHook = async (ctx) => {
     throw new GeneralError('Error in issuerAuthCredential hook: defaultIssuerEntity param is not set. Did you forget to run the getDefaultIssuerEntity hook first?');
   }
 
-  // set the version value to the default 1.0.0 is not present in the request headers
-  let version = '1.0.0';
-  if (params.headers?.version) {
-    version = params.headers?.version;
-  }
-
   // issue a DemoAuthCredential using the server sdk
   const authCredentialSubject = buildAuthCredentialSubject(did, id as string, result.email);
-  const issuerDto = await issueCredential(defaultIssuerEntity, authCredentialSubject, 'DemoAuthCredential', version);
+  const issuerDto = await issueCredentialV3(defaultIssuerEntity, authCredentialSubject, 'DemoAuthCredential');
 
   // store the issued credential
   const credentialDataService = ctx.app.service('credentialData') as MikroOrmService<CredentialEntity>;
-  const credentialEntityOptions = convertUnumDtoToCredentialEntityOptions(issuerDto, version);
-  try {
-    await credentialDataService.create(credentialEntityOptions);
-  } catch (e) {
-    logger.error('issueAuthCredential hook caught an error thrown by credentialDataService.create', e);
-    throw e;
-  }
-
-  // update the default issuer's auth token if it has been reissued
-  if (issuerDto.authToken !== defaultIssuerEntity.authToken) {
-    const issuerDataService = ctx.app.service('issuerData');
-    try {
-      await issuerDataService.patch(defaultIssuerEntity.uuid, { authToken: issuerDto.authToken });
-    } catch (e) {
-      logger.error('issueAuthCredential hook caught an error thrown by issuerDataService.patch', e);
-      throw e;
-    }
-  }
-
-  return ctx;
-};
-
-export const issueAuthCredentialV3: UserServiceHook = async (ctx) => {
-  const { id, data, result, params } = ctx;
-  const defaultIssuerEntity = params.defaultIssuerEntity as IssuerEntity;
-
-  if (!data || !id || !result) {
-    throw new BadRequest();
-  }
-
-  // only run this hook if the did is being updated
-  const { did } = data;
-  if (!did) {
-    return ctx;
-  }
-
-  if (!defaultIssuerEntity) {
-    throw new GeneralError('Error in issuerAuthCredential hook: defaultIssuerEntity param is not set. Did you forget to run the getDefaultIssuerEntity hook first?');
-  }
-
-  // set the version value to the default 1.0.0 is not present in the request headers
-  let version = '1.0.0';
-  if (params.headers?.version) {
-    version = params.headers?.version;
-  }
-
-  // issue a DemoAuthCredential using the server sdk
-  const authCredentialSubject = buildAuthCredentialSubject(did, id as string, result.email);
-  // const issuerDto = await issueCredential(defaultIssuerEntity, authCredentialSubject, 'DemoAuthCredential', version);
-  const issuerDto = await issueCredentialV3(defaultIssuerEntity, authCredentialSubject, 'DemoAuthCredential', version);
-
-  // store the issued credential
-  const credentialDataService = ctx.app.service('credentialData') as MikroOrmService<CredentialEntity>;
-  const credentialEntityOptions = convertUnumDtoToCredentialEntityOptionsV3(issuerDto, version);
+  const credentialEntityOptions = convertUnumDtoToCredentialEntityOptionsV3(issuerDto);
   try {
     await credentialDataService.create(credentialEntityOptions);
   } catch (e) {
@@ -332,70 +262,6 @@ export const issueAuthCredentialV3: UserServiceHook = async (ctx) => {
 };
 
 export const issueKYCCredential: UserServiceHook = async (ctx) => {
-  const { params } = ctx;
-
-  return issueKYCCredentialV3(ctx);
-  // if (lt(params.headers?.version, '3.0.0')) {
-  //   return issueKYCCredentialV2(ctx);
-  // } else {
-  //   return issueKYCCredentialV3(ctx);
-  // }
-};
-
-// export const issueKYCCredentialV2: UserServiceHook = async (ctx) => {
-//   const { id, data, params, result } = ctx;
-//   const defaultIssuerEntity = params.defaultIssuerEntity as IssuerEntity;
-
-//   if (!data || !id || !result) {
-//     throw new BadRequest();
-//   }
-
-//   // only run this hook if the did is being updated
-//   const { did } = data;
-//   if (!did) {
-//     return ctx;
-//   }
-
-//   if (!defaultIssuerEntity) {
-//     throw new GeneralError('Error in issueKYCCredential hook: defaultIssuerEntity param is not set. Did you forget to run the getDefaultIssuerEntity hook first?');
-//   }
-
-//   // set the version value to the default 1.0.0 is not present in the request headers
-//   let version = '1.0.0';
-//   if (params.headers?.version) {
-//     version = params.headers?.version;
-//   }
-
-//   // issue a DemoAuthCredential using the server sdk
-//   const KYCCredentialSubject = buildKYCCredentialSubject(did, result.firstName as string || 'Richard');
-//   const issuerDto = await issueCredential(defaultIssuerEntity, KYCCredentialSubject, 'KYCCredential', version);
-
-//   // store the issued credential
-//   const credentialDataService = ctx.app.service('credentialData') as MikroOrmService<CredentialEntity>;
-//   const credentialEntityOptions = convertUnumDtoToCredentialEntityOptions(issuerDto, version);
-
-//   try {
-//     await credentialDataService.create(credentialEntityOptions);
-//   } catch (e) {
-//     logger.error('issueKYCCredential hook caught an error thrown by credentialDataService.create', e);
-//     throw e;
-//   }
-
-//   // update the default issuer's auth token if it has been reissued
-//   if (issuerDto.authToken !== defaultIssuerEntity.authToken) {
-//     const issuerDataService = ctx.app.service('issuerData');
-//     try {
-//       await issuerDataService.patch(defaultIssuerEntity.uuid, { authToken: issuerDto.authToken });
-//     } catch (e) {
-//       logger.error('issueKYCCredential hook caught an error thrown by issuerDataService.patch', e);
-//       throw e;
-//     }
-//   }
-
-//   return ctx;
-// };
-
-export const issueKYCCredentialV3: UserServiceHook = async (ctx) => {
   const { id, data, params, result } = ctx;
   const defaultIssuerEntity = params.defaultIssuerEntity as IssuerEntity;
 
@@ -413,21 +279,13 @@ export const issueKYCCredentialV3: UserServiceHook = async (ctx) => {
     throw new GeneralError('Error in issueKYCCredential hook: defaultIssuerEntity param is not set. Did you forget to run the getDefaultIssuerEntity hook first?');
   }
 
-  // set the version value to the default 1.0.0 is not present in the request headers
-  let version = '3.0.0';
-  if (params.headers?.version) {
-    version = params.headers?.version;
-  }
-
   // issue a DemoAuthCredential using the server sdk
   const KYCCredentialSubject = buildKYCCredentialSubject(did, result.firstName as string || 'Richard');
-  // const issuerDto = await issueCredential(defaultIssuerEntity, KYCCredentialSubject, 'KYCCredential', version);
-  const issuerDto = await issueCredentialV3(defaultIssuerEntity, KYCCredentialSubject, 'KYCCredential', version);
+  const issuerDto = await issueCredentialV3(defaultIssuerEntity, KYCCredentialSubject, 'KYCCredential');
 
   // store the issued credential
   const credentialDataService = ctx.app.service('credentialData') as MikroOrmService<CredentialEntity>;
-  // const credentialEntityOptions = convertUnumDtoToCredentialEntityOptions(issuerDto, version);
-  const credentialEntityOptions = convertUnumDtoToCredentialEntityOptionsV3(issuerDto, version);
+  const credentialEntityOptions = convertUnumDtoToCredentialEntityOptionsV3(issuerDto);
   try {
     await credentialDataService.create(credentialEntityOptions);
   } catch (e) {
