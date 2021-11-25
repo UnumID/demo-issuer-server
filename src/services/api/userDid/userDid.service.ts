8e1b5ef3b9443a28387b0613811ed12900f67109
Ray Smets <rayjsmets@gmail.com>
2021-11-24 16:52:07 -0800

    refactor

--- src/services/api/userDid/userDid.service.ts
+++ src/services/api/userDid/userDid.service.ts
@@ -2,8 +2,8 @@ import { ServiceAddons } from '@feathersjs/feathers';
 
 import { Application } from '../../../declarations';
 import { CredentialEntity } from '../../../entities/Credential';
-import { hooks } from './credentialRequest.hooks';
-import { CredentialRequestService } from './credentialRequest.class';
+import { hooks } from './userDid.hooks';
+import { CredentialRequestService } from './userDid.class';
 
 // add this service to the service type index
 declare module '../../../declarations' {
