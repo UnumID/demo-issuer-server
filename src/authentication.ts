8d3890156c788568e61efa0a50e68453259d2578
Ray Smets <rayjsmets@gmail.com>
2022-01-13 17:23:49 -0800

    [User] userCode generated at time of login.

--- src/authentication.ts
+++ src/authentication.ts
@@ -1,9 +1,12 @@
-import { ServiceAddons } from '@feathersjs/feathers';
-import { AuthenticationService, JWTStrategy } from '@feathersjs/authentication';
+import { Params, Query, ServiceAddons } from '@feathersjs/feathers';
+import { AuthenticationResult, AuthenticationService, JWTStrategy } from '@feathersjs/authentication';
 import { LocalStrategy } from '@feathersjs/authentication-local';
 import { expressOauth } from '@feathersjs/authentication-oauth';
 
 import { Application } from './declarations';
+import { User } from './entities/User';
+import { v4 } from 'uuid';
+import logger from './logger';
 
 declare module './declarations' {
   interface ServiceTypes {
@@ -11,11 +14,35 @@ declare module './declarations' {
   }
 }
 
+/**
+ * Customized LocalStrategy to inject the userCode at time of login
+ * ref: https://docs.feathersjs.com/api/authentication/local.html#customization
+ */
+class MyLocalStrategy extends LocalStrategy {
+  app: Application;
+  constructor (app: Application) {
+    super();
+    this.app = app;
+  }
+
+  async getEntity (authenticatedUser: User, params: Params) {
+    // create and persist a userCode on the User entity before returning it
+    const userDataService = this.app.service('userData');
+    try {
+      const patchedUser = await userDataService.patch(authenticatedUser.uuid, { userCode: v4() }, params);
+      return patchedUser;
+    } catch (e) {
+      logger.error(`error in login\'s UserDataService.patch. ${e}`);
+      throw e;
+    }
+  }
+}
+
 export default function (app: Application): void {
   const authentication = new AuthenticationService(app);
 
   authentication.register('jwt', new JWTStrategy());
-  authentication.register('local', new LocalStrategy());
+  authentication.register('local', new MyLocalStrategy(app));
 
   app.use('/authentication', authentication);
   app.configure(expressOauth());
