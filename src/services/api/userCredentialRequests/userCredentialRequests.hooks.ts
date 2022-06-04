c4f5a946fe538d1973684b20bcbfd8b9bbf6c171
Ray Smets <rayjsmets@gmail.com>
2022-06-03 20:57:53 -0700

    [UserDidAssociation] modified to better match the new functionality of potentially handling only userDidAssociation requests. (#61)

--- src/services/api/userCredentialRequests/userCredentialRequests.hooks.ts
+++ src/services/api/userCredentialRequests/userCredentialRequests.hooks.ts
@@ -2,11 +2,28 @@ import { handleUserDidAssociation } from '../../hooks/handleUserDidAssociation';
 import { validateCredentialRequest } from '../../hooks/validateCredentialRequest';
 import { config } from '../../../config';
 import { getIssuerEntity } from '../../hooks/getIssuerEntity';
+import { SubjectCredentialRequestsEnrichedDto } from '@unumid/types';
+import { BadRequest } from '@feathersjs/errors';
+import { Hook } from '@feathersjs/feathers';
+
+const validateUserCredentialRequest: Hook = async (ctx) => {
+  const data = ctx.data as SubjectCredentialRequestsEnrichedDto;
+
+  if (!data) {
+    throw new BadRequest('Invalid body must be defined.');
+  }
+  
+  if (!data.credentialRequestsInfo && !data.userDidAssociation) {
+    throw new BadRequest('Invalid body: userDidAssociation or credentialRequestsInfo must be defined.');
+  }
+
+  return ctx;
+};
 
 export const hooks = {
   before: {
     all: [validateCredentialRequest],
-    create: [getIssuerEntity(config.DEFAULT_ISSUER_DID), handleUserDidAssociation]
+    create: [getIssuerEntity(config.DEFAULT_ISSUER_DID), validateUserCredentialRequest, handleUserDidAssociation]
   },
   after: {}
 };
