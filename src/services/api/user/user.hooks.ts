c6c00c8f1922b4cb3603e92a76908ac0cba9789b
Ray Smets <rayjsmets@gmail.com>
2022-08-22 15:47:47 -0700

    [IssueCredentials] updated to follow defined schemas.

--- src/services/api/user/user.hooks.ts
+++ src/services/api/user/user.hooks.ts
@@ -1,6 +1,6 @@
 import { BadRequest, GeneralError } from '@feathersjs/errors';
 import { Hook } from '@feathersjs/feathers';
-import { convertCredentialSubject, issueCredential as sdkIssueCredential, UnumDto } from '@unumid/server-sdk';
+import { convertCredentialSubject, UnumDto } from '@unumid/server-sdk';
 import { CredentialPb, CredentialSubject, ProofPb } from '@unumid/types';
 import { Service as MikroOrmService } from 'feathers-mikro-orm';
 
@@ -9,34 +9,11 @@ import { CredentialEntity, CredentialEntityOptions } from '../../../entities/Cre
 import logger from '../../../logger';
 import { IssuerEntity } from '../../../entities/Issuer';
 import { CredentialStatus } from '@unumid/types/build/protos/credential';
-import { AuthCredentialSubject, buildAuthCredentialSubject, buildEmailCredentialSubject, buildKYCCredentialSubject, EmailCredentialSubject, issueCredentialsHelper, KYCCredentialSubject } from '../../../utils/credentials';
+import { AuthCredentialSubject, buildAuthCredentialSubject, buildEmailCredentialSubject, buildFirstNameCredentialSubject, buildKYCCredentialSubject, EmailCredentialSubject, FirstNameCredentialSubject, issueCredentialsHelper, KYCCredentialSubject } from '../../../utils/credentials';
 import { formatBearerToken } from '../../../utils/formatBearerToken';
 import { convertCredentialToCredentialEntityOptions } from '../../../utils/converters';
 
 type UserServiceHook = Hook<User>;
-
-export const issueCredential = async (
-  issuerEntity: IssuerEntity,
-  credentialSubject: CredentialSubject,
-  credentialType: string
-): Promise<UnumDto<CredentialPb>> => {
-  let authCredentialResponse;
-
-  try {
-    authCredentialResponse = await sdkIssueCredential(
-      formatBearerToken(issuerEntity.authToken),
-      credentialType,
-      issuerEntity.issuerDid,
-      credentialSubject,
-      issuerEntity.privateKey
-    );
-
-    return authCredentialResponse as UnumDto<CredentialPb>;
-  } catch (e) {
-    logger.error('issueCredential caught an error thrown by the server sdk', e);
-    throw e;
-  }
-};
 
 export const convertUnumDtoToCredentialEntityOptions = (issuerDto: UnumDto<CredentialPb>): CredentialEntityOptions => {
   const proof: ProofPb = {
@@ -102,9 +79,9 @@ export const issueAuthAndKYCAndEmailCredentials: UserServiceHook = async (ctx) =
   // issue a DemoAuthCredential & KYCCredential using the server sdk
   const authCredentialSubject: AuthCredentialSubject = buildAuthCredentialSubject(did, id as string, result.email);
   const emailCredentialSubject: EmailCredentialSubject = buildEmailCredentialSubject(did, result.email);
-  const KYCCredentialSubject: KYCCredentialSubject = buildKYCCredentialSubject(did, result.firstName as string || 'Richard');
+  const fistNameCredentialSubject: FirstNameCredentialSubject = buildFirstNameCredentialSubject(did, result.firstName as string || 'Richard');
 
-  const issuerDto: UnumDto<CredentialPb[]> = await issueCredentialsHelper(defaultIssuerEntity, did, [authCredentialSubject, emailCredentialSubject, KYCCredentialSubject]);
+  const issuerDto: UnumDto<CredentialPb[]> = await issueCredentialsHelper(defaultIssuerEntity, did, [authCredentialSubject, emailCredentialSubject, fistNameCredentialSubject]);
 
   // store the issued credentials
   const credentials: CredentialPb[] = issuerDto.body;
@@ -128,100 +105,6 @@ export const issueAuthAndKYCAndEmailCredentials: UserServiceHook = async (ctx) =
       await issuerDataService.patch(defaultIssuerEntity.uuid, { authToken: issuerDto.authToken });
     } catch (e) {
       logger.error('issueAuthAndKYCAndEmailCredentials hook caught an error thrown by issuerDataService.patch', e);
-      throw e;
-    }
-  }
-
-  return ctx;
-};
-
-export const issueAuthCredential: UserServiceHook = async (ctx) => {
-  const { id, data, result, params } = ctx;
-  const defaultIssuerEntity = params.defaultIssuerEntity as IssuerEntity;
-
-  if (!data || !id || !result) {
-    throw new BadRequest();
-  }
-
-  // only run this hook if the did is being updated
-  const { did } = data;
-  if (!did) {
-    return ctx;
-  }
-
-  if (!defaultIssuerEntity) {
-    throw new GeneralError('Error in issuerAuthCredential hook: defaultIssuerEntity param is not set. Did you forget to run the getDefaultIssuerEntity hook first?');
-  }
-
-  // issue a DemoAuthCredential using the server sdk
-  const authCredentialSubject = buildAuthCredentialSubject(did, id as string, result.email);
-  const issuerDto = await issueCredential(defaultIssuerEntity, authCredentialSubject, 'DemoAuthCredential');
-
-  // store the issued credential
-  const credentialDataService = ctx.app.service('credentialData') as MikroOrmService<CredentialEntity>;
-  const credentialEntityOptions = convertUnumDtoToCredentialEntityOptions(issuerDto);
-
-  try {
-    await credentialDataService.create(credentialEntityOptions);
-  } catch (e) {
-    logger.error('issueAuthCredential hook caught an error thrown by credentialDataService.create', e);
-    throw e;
-  }
-
-  // update the default issuer's auth token if it has been reissued
-  if (issuerDto.authToken !== defaultIssuerEntity.authToken) {
-    const issuerDataService = ctx.app.service('issuerData');
-    try {
-      await issuerDataService.patch(defaultIssuerEntity.uuid, { authToken: issuerDto.authToken });
-    } catch (e) {
-      logger.error('issueAuthCredential hook caught an error thrown by issuerDataService.patch', e);
-      throw e;
-    }
-  }
-
-  return ctx;
-};
-
-export const issueKYCCredential: UserServiceHook = async (ctx) => {
-  const { id, data, params, result } = ctx;
-  const defaultIssuerEntity = params.defaultIssuerEntity as IssuerEntity;
-
-  if (!data || !id || !result) {
-    throw new BadRequest();
-  }
-
-  // only run this hook if the did is being updated
-  const { did } = data;
-  if (!did) {
-    return ctx;
-  }
-
-  if (!defaultIssuerEntity) {
-    throw new GeneralError('Error in issueKYCCredential hook: defaultIssuerEntity param is not set. Did you forget to run the getDefaultIssuerEntity hook first?');
-  }
-
-  // issue a KYCCredential using the server sdk
-  const KYCCredentialSubject = buildKYCCredentialSubject(did, result.firstName as string || 'Richard');
-  const issuerDto = await issueCredential(defaultIssuerEntity, KYCCredentialSubject, 'KYCCredential');
-
-  // store the issued credential
-  const credentialDataService = ctx.app.service('credentialData') as MikroOrmService<CredentialEntity>;
-  const credentialEntityOptions = convertUnumDtoToCredentialEntityOptions(issuerDto);
-
-  try {
-    await credentialDataService.create(credentialEntityOptions);
-  } catch (e) {
-    logger.error('issueKYCCredential hook caught an error thrown by credentialDataService.create', e);
-    throw e;
-  }
-
-  // update the default issuer's auth token if it has been reissued
-  if (issuerDto.authToken !== defaultIssuerEntity.authToken) {
-    const issuerDataService = ctx.app.service('issuerData');
-    try {
-      await issuerDataService.patch(defaultIssuerEntity.uuid, { authToken: issuerDto.authToken });
-    } catch (e) {
-      logger.error('issueKYCCredential hook caught an error thrown by issuerDataService.patch', e);
       throw e;
     }
   }
